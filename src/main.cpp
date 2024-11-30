723c838134f55d147b0e7f77ddb477d890ff8bb6
Ryanwxn <42436405+Ryanwxn@users.noreply.github.com>
2024-11-30 20:15:41 +0800

    Update main.cpp (just for test)

--- src/main.cpp
+++ src/main.cpp
@@ -43,7 +43,7 @@ void REPL()
     while (1)
     {   
         #ifndef ONLINE_JUDGE
-        printf("scm> ");
+        
         #endif
         Syntax stx = readSyntax(std :: cin); // read
         try
